c67415921c544ddf212831b58799ce0ae1e9359e
naufaliqbal <naufalopak@gmail.com>
2019-10-15 09:49:57 +0700

    add manifest and apple icon for gh pages

---
+++ precache-manifest.cb8bfdbb3caa54a61ecff5c7d684b5db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa483139de0a8e0b3c5630190f029c7",
+    "url": "calculator-128.png"
+  },
+  {
+    "revision": "4a14a169f8d8adbec2e10b40e6ebfc8f",
+    "url": "index.html"
+  },
+  {
+    "revision": "872c934d9928c1d09332",
+    "url": "main.css"
+  },
+  {
+    "revision": "872c934d9928c1d09332",
+    "url": "main.js"
+  },
+  {
+    "revision": "ea2cf81af13c1ec2d5c5",
+    "url": "runtime~main.js"
+  },
+  {
+    "revision": "2da5606ae3215627d62e",
+    "url": "vendors~main.js"
+  }
+]);
